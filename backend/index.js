c2e1cb0247abac21ccd1dfaef07821ba548341a4
D JAI <tsdineshjai@gmail.com>
2024-08-06 11:44:53 +0530

    createChat and saving the chat to chatHistory into the database logic was added

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
 import ImageKit from "imagekit";
+
+import Chat from "./models/chats.js";
+import UserChats from "./models/userChats.js";
 
 const PORT = process.env.PORT || 3000;
 
@@ -49,6 +51,61 @@ app.get("/api/upload", function (req, res) {
 
 app.get("/", (req, res) => {
 	res.send("it works and works");
+});
+
+app.post("/api/chats", async (req, res) => {
+	const { userId, text } = req.body;
+
+	try {
+		// CREATE A NEW CHAT
+		const newChat = new Chat({
+			userId: userId,
+			history: [{ role: "user", parts: [{ text }] }],
+		});
+
+		const savedChat = await newChat.save();
+		console.log(`chat is created successfully`);
+
+		// CHECK IF THE USERCHATS EXISTS,SO THAT WE CAN UPDATE THE NEW CHAT
+		const userChats = await UserChats.find({ userId: userId });
+
+		// IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+		if (!userChats.length) {
+			const newUserChats = new UserChats({
+				userId: userId,
+				chats: [
+					{
+						_id: savedChat._id,
+						title: text.substring(0, 40),
+					},
+				],
+			});
+
+			await newUserChats.save();
+
+			console.log(`newUSerChats is created successfully`, newUserChats._id);
+		} else {
+			// IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+			await UserChats.updateOne(
+				{ userId: userId },
+				{
+					$push: {
+						chats: {
+							_id: savedChat._id,
+							title: text.substring(0, 40),
+						},
+					},
+				}
+			);
+
+			res.status(201).send(newChat._id);
+
+			console.log(`newUSerChats is updated successfully`, newUserChats._id);
+		}
+	} catch (err) {
+		console.log(`error has occured`);
+		res.status(500).send(`Error creating Chat!`);
+	}
 });
 
 app.listen(PORT, () => {
